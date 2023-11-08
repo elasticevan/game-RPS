f9a803cc6650f9c32bdaa9c81f1078bc0ff23c6b
Evan Barbeyto <evann.bar@gmail.com>
2023-11-07 21:44:53 -0800

    replace console.log to DOM

--- script.js
+++ script.js
@@ -11,27 +11,32 @@ function getComputerChoice() {
     return randomChoice
 }
 
-function playerChoice() {
-    playerInput = prompt('Please enter "rock", "paper", or "scissor"!')
-    playerMove = playerInput.toLowerCase();
-        if (playerMove === '' || playerMove === null || !moves.includes(playerMove)) {
-            return 'Must input "rock", "paper", or "scissors!"'
-        } else {
-            return `${playerMove}`
-        }
-    }
+let playerScore = 0
+let computerScore = 0
+let playCount = 0
 
-let playerScore = ''
-let computerScore = ''
-let playCount = ''
-let gameCount = ''
+
+
+
+let computerSelection;
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {  
+        playerSelection = button.id;
+        computerSelection = getComputerChoice();
+        game();
+    })})
+
+const round = document.createElement('div')
+document.body.appendChild(round)
+const player = document.createElement('div')
+document.body.appendChild(player)
+const computer = document.createElement('div')
+document.body.appendChild(computer)
+
 
 function playRound() {
-    const playerSelection = playerChoice();
-    const computerSelection = getComputerChoice();
-    if (!moves.includes(playerSelection)) {
-        return 'Must input "rock", "paper", or "scissors!"'
-    } else {
         playCount++;
         if (winningMove[playerSelection] === computerSelection) {
             playerScore++;
@@ -42,29 +47,41 @@ function playRound() {
         } else {
             return 'Draw!'
         }
-    } 
-}
+    }
+
+
+const result = document.createElement('div');
+const gameResult = document.createElement('div');
+const gameOver = document.createElement('div');
+document.body.appendChild(result)
+document.body.appendChild(gameResult)
+document.body.appendChild(gameOver)
 
 function game(){
-    for (let i = 1; i <= 5; i++) {
         const roundResult = playRound();
-        if (roundResult === 'Must input "rock", "paper", or "scissors!"') {
-            i--;
-        } else {
-        console.log(`Round ${i}: ${roundResult}`)
-        }
-    }
-    console.log(`Final Score - Player: ${playerScore}, Computer: ${computerScore}`);
-    if (playerScore > computerScore) {
-        console.log("player wins game")
-    } else if (computerScore > playerScore) {
-        console.log('computer wins game')
-    } else {
-        console.log('tied game')
-    }
+        result.textContent = (`${roundResult}`);
+        round.textContent = `Round: ${playCount}`;
+        player.textContent = `Player: ${playerScore}`;
+        computer.textContent = `Computer: ${computerScore}`;
+        if (playCount === 5) {
+            if (playerScore > computerScore) {
+                gameResult.textContent = "player wins game"
+            } else if (computerScore > playerScore) {
+                gameResult.textContent = 'computer wins game'
+            } else {
+                gameResult.textContent = 'tied game'
+            } 
+            
+            gameOver.textContent = ("Game has restarted")
+            resetGame();
+}
 }
 
-game();
-console.log(playCount)
+function resetGame() {
+    playCount = 0;
+    playerScore = 0;
+    computerScore = 0; 
+}
+
 
 
